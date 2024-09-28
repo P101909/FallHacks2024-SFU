893d7a23502d81e5cf0d54b9f21d1cb3fb0a152e
Parsa Ghaderi <parsaghaderi1381@gmail.com>
2024-09-28 11:45:24 -0700

    few change

---
+++ BackEnd/utils.py
@@ -0,0 +1,11 @@
+import json
+from youtube_transcript_api import YouTubeTranscriptApi
+
+# Function to get transcript from a YouTube link
+def get_transcript(youtube_url: str) -> str:
+    video_id = youtube_url.split("v=")[-1]  # Extract video ID from URL
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    transcript_text = " ".join([entry['text'] for entry in transcript])
+    
+    return transcript_text
+
